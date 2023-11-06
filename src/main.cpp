fee4594eb4a88ca27362bf945f578a94132674bc
jlsalvat <salvat@unice.fr>
2023-11-06 17:33:57 +0100

    commit2

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <mbed.h>
-DigitalOut led (LED1);
-int main(){
-    while(1){
-    led=1;
-wait_us(1000000);
-led=0;
-wait_us(1000000);
+DigitalOut led(LED1);
+int main()
+{
+    while (1)
+    {
+        led = 1;
+        wait_us(1000000);
+        led = 0;
+        wait_us(100000);
     }
-
-
 }
